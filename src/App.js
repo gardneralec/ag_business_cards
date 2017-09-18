748340faf64cba11db4ece3a1097d5b91cb73c71
Alec Gardner <alexander.gardner731@gmail.com>
2017-09-18 12:01:56 -0500

    Foundational Changes
    Drafted groundwork for content sections corresponding with aspect ratio for US business card at 300ppi (600x1050).

--- src/App.js
+++ src/App.js
@@ -3,23 +3,59 @@ import logo from './logo.svg';
 
 import styled from 'styled-components';
 
-const Header = Header.div ,
-  text-align: center;
-  font-color: red;
-,
+const PageContainer = styled.div `
+  width: 100vw;
+  height: 100vh;
+  background-color: #eeeeee;
+`;
+
+const CardContainer = styled.div `
+  width: 600px;
+  height: 1050px;
+  max-width: 600px;
+  max-height: 1050px;
+  margin: auto auto;
+  display: block;
+  border: 1px solid #bbbbbb;
+`;
+
+const Header = styled.div `
+  width: 600px;
+  height: 350px;
+  background-color: blue;
+`;
+
+const Callout = styled.div `
+  height: 75px;
+  width: 600px;
+  background-color: lightblue;
+`;
+
+const Body = styled.div `
+  height: 500px;
+  width: 600px;
+  background-color: white;
+`;
+
+const Footer = styled.div `
+  height: 50px;
+  width: 600px;
+  background-color: white;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <PageContainer>
+        <CardContainer>
+          <Header>
+          </Header>
+          <Callout></Callout>
+          <Body></Body>
+          <Callout></Callout>
+          <Footer/>
+        </CardContainer>
+      </PageContainer>
     );
   }
 }
